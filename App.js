e09c4a2f789c086e153fd5df5d78bfc75f9171b7
Aouis <aouiscb@gmail.com>
2021-11-29 17:33:23 +0100

    screen

--- App.js
+++ App.js
@@ -1,15 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  FlatList,
+  ActivityIndicator,
+  SafeAreaView
+} from 'react-native';
 
-export default function App() {
+// Screens
+import Home from './screens/home'
+import Profile from './screens/profile'
+import Recents from './screens/recents'
+
+// Lien API
+// https://api.themoviedb.org/3/movie/top_rated?api_key=ecc51c8258646afe0ddd18a4d8eeedd8
+const movieURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=ecc51c8258646afe0ddd18a4d8eeedd8"
+
+const App = () => {
+
+  // SETTERS
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  // Récupération data
+  useEffect( () => {
+    fetch(movieURL)
+    .then((res) => res.json())
+    .then((json) => {
+      setData(json.results);
+    })
+    .catch((error) => alert(error))
+    .finally(() => setLoading(false))
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Image
+          style={styles.principal_image}
+          source={{
+            uri: 'https://image.tmdb.org/t/p/w400/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg',
+          }}
+        />
+      </View>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({id}, index)=> id}
+          renderItem={({item}) =>(
+            <View>
+              <Text>{item.id}. {item.title} - {item.original_language}</Text>
+            </View>
+          )}
+        />
+      )
+
+      }
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +71,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  principal_image: {
+    width: 180,
+    height: 250,
+  },
 });
+
+export default App;
---
+++ screens/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {StyleSheet,Text,View} from 'react-native';
+
+const Home = () => {
+    return (
+        <View style={StyleSheet.container}>
+            <Text>This is Home</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+});
+
+export default Home;
