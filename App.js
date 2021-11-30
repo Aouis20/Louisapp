210e39d29a51366449235546245d11503098a85b
Aouis <aouiscb@gmail.com>
2021-11-30 10:03:42 +0100

    Navbar&styleTODO

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import {
   ActivityIndicator,
   SafeAreaView
 } from 'react-native';
+// import {MaterialCommunityIcons} from '@expo/vector_icons'
+// import { NavigationContainer } from '@react-navigation/native'
 
 // Screens
 import Home from './screens/home'
@@ -15,9 +17,16 @@ import Profile from './screens/profile'
 import Recents from './screens/recents'
 
 // Lien API
-// https://api.themoviedb.org/3/movie/top_rated?api_key=ecc51c8258646afe0ddd18a4d8eeedd8
-const movieURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=ecc51c8258646afe0ddd18a4d8eeedd8"
+// BASE_URL : https://api.themoviedb.org/3
+// KEY : ?api_key=ecc51c8258646afe0ddd18a4d8eeedd8
 
+// TOP_RATED : movie/top_rated
+// DETAILS_MOVIE : /movie/{movie_id}
+
+const movieURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=ecc51c8258646afe0ddd18a4d8eeedd8"
+// const Tab = createMaterialBottomTabNavigator();
+
+// APP
 const App = () => {
 
   // SETTERS
@@ -36,15 +45,53 @@ const App = () => {
   }, []);
 
   return (
+
     <SafeAreaView style={styles.container}>
-      <View>
-        <Image
-          style={styles.principal_image}
-          source={{
-            uri: 'https://image.tmdb.org/t/p/w400/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg',
-          }}
-        />
-      </View>
+      
+        {/*
+        <NavigationContainer>
+          <Tab.Navigator
+          initialRoute="Home"
+          activeColor="#02ad94"
+          inactiveColor="#dedede"
+          style={{backgroundColor: '#000'}}
+          barStyle={{backgroundColor: '#0f0f0f', padding: 4}}
+          >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons name = 'home' color={color} zise={28} />
+              )
+            }}
+          />
+            
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons name = 'camera-matering-spot' color={color} zise={28} />
+              )
+            }}
+          />
+
+          <Tab.Screen
+            name="Recents"
+            component={Recents}
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons name = 'account' color={color} zise={28} />
+              )
+            }}
+          />
+          </Tab.Navigator>
+        </NavigationContainer>
+        */}
       {isLoading ? (
         <ActivityIndicator />
       ) : (
@@ -53,6 +100,14 @@ const App = () => {
           keyExtractor={({id}, index)=> id}
           renderItem={({item}) =>(
             <View>
+              {/*
+              <Image
+                style={styles.principal_image}
+                source={{
+                  uri: 'https://image.tmdb.org/t/p/w500/'+({item.poster_path}.toString()),
+                }}
+              />
+              */}
               <Text>{item.id}. {item.title} - {item.original_language}</Text>
             </View>
           )}
